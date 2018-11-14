a3cf6c42107cc39c09a686e41d8f3b1e141c9107
Jiang <c5247693@c02pxbsrg8wn.dhcp.ctul.sap.corp>
2018-11-14 15:35:45 +0800

    init project

---
+++ app/src/main/java/com/example/demo/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demo.nerdlauncher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NerdLauncherActivity : BaseFragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+    }
+}
